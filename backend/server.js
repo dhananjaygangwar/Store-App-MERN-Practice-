47ae96fcbdb76e5b59772cfea1ecf537df124095
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-08-08 04:54:59 +0530

    Fixed Errors Mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -12,6 +16,7 @@ app.get("/products", (req,res) => {});
 console.log(process.env.MONGO_URI);
 
 app.listen(5000,() => {
+    connectDB();
     console.log("Server started at http://localhost:5000")
 })
 
