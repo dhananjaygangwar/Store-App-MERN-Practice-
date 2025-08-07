e768537aab763ed20a87adecdc605cb451714464
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-08-08 04:14:28 +0530

    same

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ import express from 'express';
 
 const app = express();
 
-app.get("",(req,res) => {
-    res.send("Server is ready");
-})
+// app.get("",(req,res) => {
+//     res.send("Server is ready");
+// });
 
+
+app.get("/products", (req,res) => {});
+
+console.log(process.env.MONGO_URI);
 
 app.listen(5000,() => {
     console.log("Server started at http://localhost:5000")
