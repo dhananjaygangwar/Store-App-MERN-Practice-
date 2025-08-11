28bcb1d5fcc5dea067bb72ecf84e0c457d7e0397
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-08-12 02:17:50 +0530

    Update 12 Aug 2025

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,17 @@ import productRoutes from "./routes/product.route.js"
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json());//middlewar for json data in body
 
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
 
 
 
-app.listen(5000,() => {
+app.listen(PORT,() => {
     connectDB();
-    console.log("Server started at http://localhost:5000")
-})
+    console.log("Server started at http://localhost:" + PORT)
+});
 
