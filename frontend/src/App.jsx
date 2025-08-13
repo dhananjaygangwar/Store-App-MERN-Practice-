2c90514f1a753974635bf747ca91d9c58bc70d49
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-08-13 05:38:53 +0530

    Component changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,9 @@
-import { Button } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 
 function App() {
@@ -6,7 +11,7 @@ function App() {
 
   return (
     <Box minH={"100vh"}>
-      {/* <navbar/> */}
+      <Navbar/>
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/create" element={<CreatePage/>} />
