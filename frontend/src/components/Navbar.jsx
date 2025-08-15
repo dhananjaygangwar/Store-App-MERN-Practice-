4ab8dd9720651f11485deb9f4d80d3b0b19e878c
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-08-15 09:01:02 +0530

    Dark mode and light Mode added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,42 +1,48 @@
-import { Button,Container, Flex, HStack, Text } from '@chakra-ui/react';
-import React from 'react';
+import { Button, Container, Flex, HStack, Text, useColorMode } from "@chakra-ui/react";
+
 import { Link } from "react-router-dom";
 import { PlusSquareIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
-  return <Container maxW={"1140px"} px={4}>
-    <Flex
-      h = {16}
-      alignItems={"center"}
-      justifyContent={"space-between"}
-      flexDir={{
-        base:"column",
-        sm:"row"
-      }}
-    >
-
-      <Text
-        fontSize={{base: "22", sm: "28"}}
-        fontWeight={"bold"}
-        textTransform={"uppercase"}
-        textAlign={"center"}
-        bgGradient={"linear(to-r, cyan.400, blue.500)"}
-        bgClip={"text"}
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
       >
-        <Link to={"/"}> Product Store 🛒 </Link>
-      </Text>
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          <Link to={"/"}> Product Store 🛒 </Link>
+        </Text>
 
-      <HStack spacing={2} alignItems={"center"}>
-        <Link to={"/create"}>
-        <Button>
-          <PlusSquareIcon fontSize={20} />
-        </Button>
-        </Link>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <PlusSquareIcon fontSize={20} />
+            </Button>
+          </Link>
 
-      </HStack>
-
-    </Flex>
-  </Container>
+          <Link>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? "🌙" : "🌞"}
+            </Button>
+          </Link>
+        </HStack>
+      </Flex>
+    </Container>
+  );
 };
 
-export default Navbar
+export default Navbar;
