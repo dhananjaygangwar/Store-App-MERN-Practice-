da06397a0938afb74dab57ff2b85d42a4eca3e3a
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-08-24 08:39:12 +0530

    minor bug fixes with toaster notification

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,13 @@
-import { Box, Button, Container, Heading, Input, useColorModeValue, useToast, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  useColorModeValue,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import { useProductStore } from "../store/product";
 
@@ -11,51 +20,73 @@ const CreatePage = () => {
 
   const toast = useToast();
 
-  const {createProduct} = useProductStore()
+  const { createProduct } = useProductStore();
 
-  const handleAddProduct = async() => {
-    const {success,message} = await createProduct(newProduct);
-    if(!success){
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+    if (!success) {
       toast({
-        "titile":"Error",
+        title: "Error",
         description: message,
         status: "error",
-      })
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        isClosable: true,
+      });
     }
   };
 
   return (
     <Container maxW={"container.sm"}>
       <VStack spacing={8}>
-        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>Create New Product</Heading>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
       </VStack>
-      <Box w={"full"} bg={useColorModeValue("white","gray.800")} p={6} rounded={"lg"} shadow={"md"}>
+      <Box
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        p={6}
+        rounded={"lg"}
+        shadow={"md"}
+      >
         <VStack spacing={4}>
           <Input
             placeholder="Product Name"
             name="name"
             value={newProduct.name}
-            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, name: e.target.value })
+            }
           />
           <Input
             placeholder="Price"
             name="price"
             value={newProduct.price}
-            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, price: e.target.value })
+            }
           />
           <Input
             placeholder="Image URL"
             name="image"
             value={newProduct.image}
-            onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, image: e.target.value })
+            }
           />
           <Button colorScheme="blue" onClick={handleAddProduct} w="full">
-           Add Product 
+            Add Product
           </Button>
         </VStack>
       </Box>
     </Container>
-  
-  )};
+  );
+};
 
-export default CreatePage
+export default CreatePage;
