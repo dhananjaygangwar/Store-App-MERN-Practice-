9eb4d1a8bdb5e5d1557a305bff68960b5f7142a9
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-08-26 20:06:11 +0530

    homepage work

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Container, Text, VStack } from '@chakra-ui/react';
+
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW='container.xl' py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={""}
+          fontWeight={""}
+          bgGradient={""}
+          bgClip={""}
+          textAlign={""}
+        >
+          Current Products 
+        </Text>
+
+      </VStack>
+    </Container>
   )
-}
+};
 
 export default HomePage
