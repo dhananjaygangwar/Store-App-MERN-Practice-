74ba5493a6315f51a8ce37f2ed8b82181901d8cc
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-08-27 20:43:04 +0530

    d

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,11 +6,11 @@ const HomePage = () => {
     <Container maxW='container.xl' py={12}>
       <VStack spacing={8}>
         <Text
-          fontSize={""}
-          fontWeight={""}
-          bgGradient={""}
-          bgClip={""}
-          textAlign={""}
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
         >
           Current Products 
         </Text>
